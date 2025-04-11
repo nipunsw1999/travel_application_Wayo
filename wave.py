bd9cc70b85160e78f2dc7b730c1a02c04d6ba649
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-04-11 05:53:51 +0530

    wave app added basic

--- wave.py
+++ wave.py
@@ -1,5 +1,16 @@
 from h2o_wave import main, app, Q, ui
 
+
+"""
+    xs: 0px for extra small devices (portrait phones)
+    s: 576px for small devices (landscape phones)
+    m: 768px for medium devices (tablets)
+    l: 992px for large devices (desktops)
+    xl: 1200px for extra large devices (large desktops).
+"""
+
+
+
 @app('/')
 async def serve(q: Q):
     if not q.client.initialized:
@@ -22,26 +33,71 @@ async def serve(q: Q):
         box='',
         theme=q.client.themeName,
         layouts=[
+            # Mobile layout
             ui.layout(
                 breakpoint='xs',
                 zones=[
                     ui.zone(name='header'),
-                    ui.zone(name='content',direction=ui.ZoneDirection.ROW, size='100%',
-                            zones=[
-                                    ui.zone(name="content-left",direction=ui.ZoneDirection.COLUMN, size='50%',
-                                            zones=[
-                                                    ui.zone(name="content-left-column",direction=ui.ZoneDirection.COLUMN)
-                                                ]),
-                                    ui.zone(name="content-right",direction=ui.ZoneDirection.COLUMN, size='50%',
-                                            zones=[
-                                                    ui.zone(name="content-right-column",direction=ui.ZoneDirection.COLUMN)
-                                                ]),
-                                ]),
+                    ui.zone(
+                        name='content',
+                        direction=ui.ZoneDirection.COLUMN,
+                        size='100%',
+                        zones=[
+                            ui.zone(
+                                name='content-left',
+                                direction=ui.ZoneDirection.COLUMN,
+                                size='50%',
+                                zones=[
+                                    ui.zone(name='content-left-column', direction=ui.ZoneDirection.COLUMN)
+                                ]
+                            ),
+                            ui.zone(
+                                name='content-right',
+                                direction=ui.ZoneDirection.COLUMN,
+                                size='50%',
+                                zones=[
+                                    ui.zone(name='content-right-column', direction=ui.ZoneDirection.COLUMN)
+                                ]
+                            ),
+                        ]
+                    ),
+                    ui.zone(name='footer'),
+                ]
+            ),
+
+            ui.layout(
+                breakpoint='l',
+                zones=[
+                    ui.zone(name='header'),
+                    ui.zone(
+                        name='content',
+                        direction=ui.ZoneDirection.ROW,
+                        size='100%',
+                        zones=[
+                            ui.zone(
+                                name='content-left',
+                                direction=ui.ZoneDirection.COLUMN,
+                                size='50%',
+                                zones=[
+                                    ui.zone(name='content-left-column', direction=ui.ZoneDirection.COLUMN)
+                                ]
+                            ),
+                            ui.zone(
+                                name='content-right',
+                                direction=ui.ZoneDirection.COLUMN,
+                                size='50%',
+                                zones=[
+                                    ui.zone(name='content-right-column', direction=ui.ZoneDirection.COLUMN)
+                                ]
+                            ),
+                        ]
+                    ),
                     ui.zone(name='footer'),
                 ]
             )
         ]
     )
+
 
     q.page['header'] = ui.header_card(
         box='header',
